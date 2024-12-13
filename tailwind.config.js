8a5c18cbdac361a1b3810a00a1e80c50beee3a16
BugHunter-X <askzenai@gmail.com>
2024-12-13 12:36:08 +0100

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+```javascript
+//This is a tailwindcss configuration file.
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'theme-color-primary': '#007bff',
+      }
+    }
+  },
+  plugins: [],
+}
+```
